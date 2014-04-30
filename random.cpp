456fca85ea8cdc9ef26b5072b6a0306b2c964fae
linked20 <edvill26@gmail.com>
2014-04-30 09:42:19 -0700

    added a cpp file

---
+++ random.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string name;
+	
+	cout << "Hello, what is your name human?"; 
+	cin >> name;
+	cout << "\nGreetings " << name << endl;
+	
+	return 0;
+	
+	}
+	
